7b7d0db4fe36f1e69f5e248846f550b08e1e68d3
JasonHuangZL <jason.huang@zerologix.com>
2026-01-30 16:12:31 +0800

    Add icon

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from src.main_ui import StockUI
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.png"))
     window = StockUI()
     window.show()
     sys.exit(app.exec())
